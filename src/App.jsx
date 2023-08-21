68d6afa7958c1a370d2b414aa80a89753cced53d
Chung-Ying <ag.cyho@gmail.com>
2023-08-21 20:44:26 +0800

    chore: add bootstrap gap

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import "./App.scss";
 function App() {
   return (
     <div className="app row">
-      <p className="col-6">Hello, world!</p>
-      <p>test test</p>
-      <h1>merge to main test</h1>
-      <h2>merge test</h2>
-      <h3>merge merge test</h3>
+      
     </div>
   );
 }
