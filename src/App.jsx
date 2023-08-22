a60a283057b647c1b7a79c5a5facab57ff5dab07
mikachou <chou1005.tw@gmail.com>
2023-08-22 12:10:17 +0800

    update BrosweRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LoginPage, RegisterPage } from "../src/pages/index";
 import Toolbar from "./components/Toolbar";
 
 function App() {
   return (
     <div className="app row">
-      <Toolbar/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="register" element={<RegisterPage />}></Route>
+          <Route path="login" element={<LoginPage />}></Route>
+          <Route path="*" element={<Toolbar />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
