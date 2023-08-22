e2a118823cd17099a4c6a92452defb5ef84055b5
mikachou <chou1005.tw@gmail.com>
2023-08-22 15:27:46 +0800

    add MainPage

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import Toolbar from "../components/Toolbar";
+import PopularBar from "../components/PopularBar";
+
+const MainPage = () => {
+  return (
+    <>
+      <Toolbar />
+      <div className="col">Here is main/profile</div>
+      <PopularBar />
+    </>
+  );
+};
+
+export default MainPage;
