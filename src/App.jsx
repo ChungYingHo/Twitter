f06723341069db4d4a11233abb7ac5a59f041390
Chung-Ying <ag.cyho@gmail.com>
2023-08-22 14:04:29 +0800

    feat: add popular-bar. / fix: the color of toolbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.scss";
 import Toolbar from "./components/Toolbar";
+import PopularBar from "./components/PopularBar";
 
 function App() {
   return (
     <div className="app row">
       <Toolbar/>
+      <div className="col">Here is main/profile</div>
+      <PopularBar/>
     </div>
   );
 }
