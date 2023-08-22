224dd9b0b876bb327f39df3e52574031ad9b574c
mikachou <chou1005.tw@gmail.com>
2023-08-22 12:08:42 +0800

    update auth.style

--- src/components/common/auth.styled.js
+++ src/components/common/auth.styled.js
@@ -10,8 +10,9 @@ const StyedContainer = styled.div`
 `;
 
 const StyledAuthInputContainer = styled.div`
-  width: 50%;
-  margin-top: 30px;
+  width: 356px;
+  height: 78px;
+  margin-top: 20px;
 `;
 
 const StyledButton = styled.button`
@@ -20,11 +21,11 @@ const StyledButton = styled.button`
   border: none;
 
   color: white;
-  min-width: 300px;
+  width: 356px;
   font-family: "Noto Sans TC", sans-serif;
   font-weight: bold;
   padding: 6px 0;
-  margin: 2rem 0;
+  margin: 2rem 0 1rem 0;
 
   &.hover {
     cursor: pointer;
@@ -35,11 +36,26 @@ const StyledLinkText = styled.div`
   color: #0062ff;
   font-size: 16px;
   font-weight: 400;
+  margin-bottom: 20px;
 `;
 
+const StyledLinkWrapper = styled.div`
+  width: 150px;
+  display: flex;
+  justify-content: space-around;
+  margin-left: 200px;
+  padding-bottom: 20px;
+`;
+
+const StyledTittle = styled.h3`
+  margin: 24px 0 20px 0;
+  font-weight: 700;
+`;
 export {
   StyedContainer as AuthContainer,
   StyledAuthInputContainer as AuthInputContainer,
   StyledButton as AuthButton,
   StyledLinkText as AuthLinkText,
+  StyledLinkWrapper as AuthLinkWrapper,
+  StyledTittle as AuthTittle,
 };
