421f823cbb8eb7ffb66dc49cfb8dcfc1a052a7b5
mikachou <chou1005.tw@gmail.com>
2023-08-22 15:28:03 +0800

    update SettingPage and MainPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import LoginPage from "./LoginPage";
 import RegisterPage from "./RegisterPage";
 import AdminLoginPage from "./AdminLoginPage";
+import MainPage from "./MainPage";
+import SettingPage from "./SettingPage";
 
-export { LoginPage, RegisterPage, AdminLoginPage };
+export { LoginPage, RegisterPage, AdminLoginPage, MainPage, SettingPage };
