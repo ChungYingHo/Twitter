2786942cda9429902f0daa50038c999de0ef3080
mikachou <chou1005.tw@gmail.com>
2023-08-23 21:18:29 +0800

    adjust NewPost

--- src/components/main/NewPost.jsx
+++ src/components/main/NewPost.jsx
@@ -1,56 +1,25 @@
-import styled from 'styled-components'
-import {ReactComponent as Close} from '../../assets/close.svg'
-import * as style from '../common/common.styled'
+import styled from "styled-components";
 
-const Container = styled.div`
-    height: 300px;
-    width: 100%;
-    border-radius: 14px;
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-    position: absolute;
-    top: 56px;
-    background-color: ${style.colors.white};
-    .header{
-        height: 41px;
-        border-bottom: ${style.styledBorder};
-        .icon{
-            margin-left: 16px;
-            cursor: pointer;
-        }
-    }
-    .info{
-        display: flex;
-        align-items: center;
-        img{
-            ${style.styledImg}
-            margin-left: 16px;
-        }
-        p{
-            margin: 0 0 0 8px;
-            font-weight: 400;
-            font-size: 16px;
-            color: #6c757d;
-        }
-    }
-    textarea{
-        width: calc(100% - 32px);
-        height: 40%;
-        resize: none;
-        border: transparent solid;
-        outline: none;
-        margin: 0 16px;
-    }
-`
+const Btn = styled.button`
+  width: 64px;
+  height: 40px;
+  border-radius: 50px;
+  border: transparent solid;
+  background-color: #ff6600;
+  font-weight: 400;
+  font-size: 16px;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  bottom: 16px;
+  right: 16px;
 
-const Btn = styled(style.StyledBtn)`
-    width: 64px;
-    height: 40px;
-    position: absolute;
-    bottom: 16px;
-    right: 16px;
-`
+  &:active {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3) inset;
+  }
+`;
 
 export default function NewPost() {
   return (
