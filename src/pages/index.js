1eff8d4d744c0969fc199268afcde99515ed9520
mikachou <chou1005.tw@gmail.com>
2023-08-23 12:47:22 +0800

    update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,13 @@ import RegisterPage from "./RegisterPage";
 import AdminLoginPage from "./AdminLoginPage";
 import MainPage from "./MainPage";
 import SettingPage from "./SettingPage";
+import UserPage from "./UserPage";
 
-export { LoginPage, RegisterPage, AdminLoginPage, MainPage, SettingPage };
+export {
+  LoginPage,
+  RegisterPage,
+  AdminLoginPage,
+  MainPage,
+  SettingPage,
+  UserPage,
+};
