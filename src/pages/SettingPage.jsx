4cd08d71582ec8377c9757a2b4829407bb4f38c1
mikachou <chou1005.tw@gmail.com>
2023-08-24 21:21:47 +0800

    adjust setting layout

--- src/pages/SettingPage.jsx
+++ src/pages/SettingPage.jsx
@@ -9,6 +9,9 @@ import {
   SettingHr,
 } from "../components/common/setting.styled";
 import { useState } from "react";
+import styled from "styled-components";
+import * as style from "../components/common/common.styled";
+import PopularBar from "../components/PopularBar";
 // import { Link } from "react-router-dom";
 
 const SettingPage = () => {
@@ -40,71 +43,82 @@ const SettingPage = () => {
     }
   };
 
+  const Container = styled.div`
+    outline: green solid 2px;
+    padding: 0;
+    width: 56.2%;
+    border: ${style.styledBorder};
+    position: relative;
+  `;
+
   return (
     <>
-      <SettingContainer>
-        <SettingTittleContainer>
-          <SettingTittle>帳戶設定</SettingTittle>
-        </SettingTittleContainer>
+      <Container>
+        <SettingContainer>
+          <SettingTittleContainer>
+            <SettingTittle>帳戶設定</SettingTittle>
+          </SettingTittleContainer>
 
-        <SettingHr />
+          <SettingHr />
 
-        <SettingInputContainer>
-          <AuthInput
-            label={"帳號"}
-            name={account}
-            value={account}
-            placeholder={"請輸入帳號"}
-            onChange={(accountInputValue) => setAccount(accountInputValue)}
-          />
-        </SettingInputContainer>
+          <SettingInputContainer>
+            <AuthInput
+              label={"帳號"}
+              name={account}
+              value={account}
+              placeholder={"請輸入帳號"}
+              onChange={(accountInputValue) => setAccount(accountInputValue)}
+            />
+          </SettingInputContainer>
 
-        <SettingInputContainer>
-          <AuthInput
-            label={"名稱"}
-            name={name}
-            value={name}
-            placeholder={"請輸入使用者名稱"}
-            onChange={(nameInputValue) => setName(nameInputValue)}
-          />
-        </SettingInputContainer>
+          <SettingInputContainer>
+            <AuthInput
+              label={"名稱"}
+              name={name}
+              value={name}
+              placeholder={"請輸入使用者名稱"}
+              onChange={(nameInputValue) => setName(nameInputValue)}
+            />
+          </SettingInputContainer>
 
-        <SettingInputContainer>
-          <AuthInput
-            label={"Email"}
-            name={email}
-            value={email}
-            placeholder={"請輸入Email"}
-            onChange={(emailInputValue) => setEmail(emailInputValue)}
-          />
-        </SettingInputContainer>
+          <SettingInputContainer>
+            <AuthInput
+              label={"Email"}
+              name={email}
+              value={email}
+              placeholder={"請輸入Email"}
+              onChange={(emailInputValue) => setEmail(emailInputValue)}
+            />
+          </SettingInputContainer>
 
-        <SettingInputContainer>
-          <AuthInput
-            label={"密碼"}
-            name={password}
-            value={password}
-            placeholder={"請輸入密碼"}
-            onChange={(passwordInputValue) => setPassword(passwordInputValue)}
-          />
-        </SettingInputContainer>
+          <SettingInputContainer>
+            <AuthInput
+              label={"密碼"}
+              name={password}
+              value={password}
+              placeholder={"請輸入密碼"}
+              onChange={(passwordInputValue) => setPassword(passwordInputValue)}
+            />
+          </SettingInputContainer>
 
-        <SettingInputContainer>
-          <AuthInput
-            label={"密碼再確認"}
-            name={checkPassword}
-            value={checkPassword}
-            placeholder={"請輸入密碼"}
-            onChange={(checkPasswordInputValue) =>
-              setCheckPassword(checkPasswordInputValue)
-            }
-          />
-        </SettingInputContainer>
+          <SettingInputContainer>
+            <AuthInput
+              label={"密碼再確認"}
+              name={checkPassword}
+              value={checkPassword}
+              placeholder={"請輸入密碼"}
+              onChange={(checkPasswordInputValue) =>
+                setCheckPassword(checkPasswordInputValue)
+              }
+            />
+          </SettingInputContainer>
 
-        <SettingButtonWrapper>
-          <SettingButton onClick={handleClick}>儲存</SettingButton>
-        </SettingButtonWrapper>
-      </SettingContainer>
+          <SettingButtonWrapper>
+            <SettingButton onClick={handleClick}>儲存</SettingButton>
+          </SettingButtonWrapper>
+        </SettingContainer>
+      </Container>
+      <PopularBar />
     </>
   );
 };
