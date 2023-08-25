34c7f326183ca3876b9b57522b6ed54cf976ff4d
mikachou <chou1005.tw@gmail.com>
2023-08-25 11:03:07 +0800

    settingPage Layout adjust

--- src/pages/SettingPage.jsx
+++ src/pages/SettingPage.jsx
@@ -1,6 +1,5 @@
 import AuthInput from "../components/AuthInput";
 import {
-  SettingContainer,
   SettingInputContainer,
   SettingTittleContainer,
   SettingTittle,
@@ -44,69 +43,67 @@ const SettingPage = () => {
   return (
     <>
       <Container>
-        <SettingContainer>
-          <SettingTittleContainer>
-            <SettingTittle>帳戶設定</SettingTittle>
-          </SettingTittleContainer>
+        <SettingTittleContainer>
+          <SettingTittle>帳戶設定</SettingTittle>
+        </SettingTittleContainer>
 
-          <SettingHr />
+        <SettingHr />
 
-          <SettingInputContainer>
-            <AuthInput
-              label={"帳號"}
-              name={account}
-              value={account}
-              placeholder={"請輸入帳號"}
-              onChange={(accountInputValue) => setAccount(accountInputValue)}
-            />
-          </SettingInputContainer>
+        <SettingInputContainer>
+          <AuthInput
+            label={"帳號"}
+            name={account}
+            value={account}
+            placeholder={"請輸入帳號"}
+            onChange={(accountInputValue) => setAccount(accountInputValue)}
+          />
+        </SettingInputContainer>
 
-          <SettingInputContainer>
-            <AuthInput
-              label={"名稱"}
-              name={name}
-              value={name}
-              placeholder={"請輸入使用者名稱"}
-              onChange={(nameInputValue) => setName(nameInputValue)}
-            />
-          </SettingInputContainer>
+        <SettingInputContainer>
+          <AuthInput
+            label={"名稱"}
+            name={name}
+            value={name}
+            placeholder={"請輸入使用者名稱"}
+            onChange={(nameInputValue) => setName(nameInputValue)}
+          />
+        </SettingInputContainer>
 
-          <SettingInputContainer>
-            <AuthInput
-              label={"Email"}
-              name={email}
-              value={email}
-              placeholder={"請輸入Email"}
-              onChange={(emailInputValue) => setEmail(emailInputValue)}
-            />
-          </SettingInputContainer>
+        <SettingInputContainer>
+          <AuthInput
+            label={"Email"}
+            name={email}
+            value={email}
+            placeholder={"請輸入Email"}
+            onChange={(emailInputValue) => setEmail(emailInputValue)}
+          />
+        </SettingInputContainer>
 
-          <SettingInputContainer>
-            <AuthInput
-              label={"密碼"}
-              name={password}
-              value={password}
-              placeholder={"請輸入密碼"}
-              onChange={(passwordInputValue) => setPassword(passwordInputValue)}
-            />
-          </SettingInputContainer>
+        <SettingInputContainer>
+          <AuthInput
+            label={"密碼"}
+            name={password}
+            value={password}
+            placeholder={"請輸入密碼"}
+            onChange={(passwordInputValue) => setPassword(passwordInputValue)}
+          />
+        </SettingInputContainer>
 
-          <SettingInputContainer>
-            <AuthInput
-              label={"密碼再確認"}
-              name={checkPassword}
-              value={checkPassword}
-              placeholder={"請輸入密碼"}
-              onChange={(checkPasswordInputValue) =>
-                setCheckPassword(checkPasswordInputValue)
-              }
-            />
-          </SettingInputContainer>
+        <SettingInputContainer>
+          <AuthInput
+            label={"密碼再確認"}
+            name={checkPassword}
+            value={checkPassword}
+            placeholder={"請輸入密碼"}
+            onChange={(checkPasswordInputValue) =>
+              setCheckPassword(checkPasswordInputValue)
+            }
+          />
+        </SettingInputContainer>
 
-          <SettingButtonWrapper>
-            <SettingButton onClick={handleClick}>儲存</SettingButton>
-          </SettingButtonWrapper>
-        </SettingContainer>
+        <SettingButtonWrapper>
+          <SettingButton onClick={handleClick}>儲存</SettingButton>
+        </SettingButtonWrapper>
       </Container>
       <PopularBar />
     </>
