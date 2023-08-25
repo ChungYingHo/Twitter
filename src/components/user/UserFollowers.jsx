cf61df1d23d570f831ed5433aee39e64a14882d1
mikachou <chou1005.tw@gmail.com>
2023-08-25 11:54:03 +0800

    update header link

--- src/components/user/UserFollowers.jsx
+++ src/components/user/UserFollowers.jsx
@@ -1,7 +1,71 @@
 import styled from "styled-components";
+import * as style from "../common/common.styled";
+import PopularBar from "../PopularBar";
+import { ReactComponent as LeftArrow } from "../../assets/left-arrow.svg";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  outline: green solid 2px;
+  padding: 0;
+  width: 56.2%;
+  border: ${style.styledBorder};
+  position: relative;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  height: 59px;
+  margin-top: 16px;
+  border-bottom: ${style.styledBorder};
+  display: flex;
+  align-items: center;
+  padding: 0 30px;
+
+  h5 {
+    font-weight: 700;
+    font-size: 18px;
+    margin: 0 0 2px 0;
+  }
+  p {
+    font-size: 13px;
+    color: #6c757d;
+  }
+`;
+
+const HeaderTittleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-left: 16px;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  margin: 0;
+  padding: 0;
+  color: #171725;
+  &:hover {
+    color: #171725;
+  }
+`;
 
 const UserFollowers = () => {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <StyledLink to="/user">
+          <Header>
+            <LeftArrow />
+            <HeaderTittleWrapper>
+              <h5>Egg Head</h5>
+              <p>25推文</p>
+            </HeaderTittleWrapper>
+          </Header>
+        </StyledLink>
+      </Container>
+      <PopularBar />
+    </>
+  );
 };
 
 export default UserFollowers;
