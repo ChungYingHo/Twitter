59c8a42891b3345902f364a7167a6178dbe76f63
Chung-Ying <ag.cyho@gmail.com>
2023-08-31 14:38:43 +0800

    delete unuse code

--- src/pages/SettingPage.jsx
+++ src/pages/SettingPage.jsx
@@ -52,26 +52,6 @@ const SettingPage = () => {
     useResetErrorsEffect()
     // 驗證 token
     useAuthValitate('/login')
-    // 抓取用戶資料
-    // useEffect(() => {
-    //   const fetchingUser = async () => {
-    //     const userId = localStorage.getItem('userID')
-    //     console.log('settingID', userId)
-    //     console.log(typeof(userId))
-    //     try {
-    //       console.log(userId)
-    //       const userData = await getUser({id: userId});
-    //       setUser(prevUser=>userData);
-    //       setAccount(prevAccount=>userData.account);
-    //       setName(prevName=>userData.name);
-    //       setEmail(prevEmail=>userData.email);
-    //       console.log('userdata',userData)
-    //     } catch (error) {
-    //       console.error("Get User Failed:", error);
-    //     }
-    //   };
-    //   fetchingUser();
-    // }, []);
 
     const handleClick = async () => {
       if (
